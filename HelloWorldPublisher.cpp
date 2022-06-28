09b1594465a6323927a888b2d9a03576c532166b
Barry Xu <barry.xu@sony.com>
2022-06-28 15:04:23 +0800

    Import code
    Signed-off-by: Barry Xu <barry.xu@sony.com>

---
+++ HelloWorldPublisher.cpp
@@ -0,0 +1,229 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldPublisher.cpp
+ *
+ */
+
+#include "HelloWorldPublisher.h"
+#include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/PublisherAttributes.h>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/publisher/qos/PublisherQos.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
+
+#include <thread>
+
+#include <fastdds/rtps/transport/UDPv4TransportDescriptor.h>
+#include <fastrtps/rtps/attributes/RTPSParticipantAttributes.h>
+
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+
+using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastdds::rtps;
+using namespace eprosima::fastrtps::rtps;
+
+HelloWorldPublisher::HelloWorldPublisher()
+    : participant_(nullptr)
+    , publisher_(nullptr)
+    , topic_(nullptr)
+    , writer_(nullptr)
+    , type_(new HelloWorldPubSubType())
+{
+}
+
+bool HelloWorldPublisher::init()
+{
+    // hello_.message("HelloWorld");
+    DomainParticipantQos pqos;
+    pqos.name("Participant_pub");
+
+    pqos.wire_protocol().builtin.discovery_config.discoveryProtocol = DiscoveryProtocol_t::SIMPLE;
+    pqos.wire_protocol().builtin.discovery_config.use_SIMPLE_EndpointDiscoveryProtocol = true;
+    pqos.wire_protocol().builtin.discovery_config.use_STATIC_EndpointDiscoveryProtocol = false;
+    pqos.wire_protocol().builtin.discovery_config.leaseDuration = Duration_t(3, 0);
+    pqos.wire_protocol().builtin.discovery_config.leaseDuration_announcementperiod = Duration_t(1, 0);
+    auto transport = std::make_shared<UDPv4TransportDescriptor>();
+    transport->TTL = 64;
+    pqos.transport().user_transports.push_back(transport);
+    pqos.transport().use_builtin_transports = false;
+
+    participant_ = DomainParticipantFactory::get_instance()->create_participant(0, pqos);
+
+    if (participant_ == nullptr)
+    {
+        return false;
+    }
+
+    //REGISTER THE TYPE
+    type_.register_type(participant_);
+
+    //CREATE THE PUBLISHER
+    DataWriterQos wqos;
+    wqos.reliability().kind = RELIABLE_RELIABILITY_QOS;
+    //
+    wqos.history().kind = KEEP_LAST_HISTORY_QOS;
+    wqos.durability().kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    wqos.endpoint().history_memory_policy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    wqos.publish_mode().kind = SYNCHRONOUS_PUBLISH_MODE;
+    wqos.reliable_writer_qos().times.heartbeatPeriod.seconds = 0;
+    wqos.reliable_writer_qos().times.heartbeatPeriod.nanosec = 500 * 1000 * 1000;
+
+    wqos.resource_limits().max_instances = 1024;
+    wqos.resource_limits().max_samples_per_instance = 1;
+    wqos.data_sharing().off();
+
+    publisher_ = participant_->create_publisher(PUBLISHER_QOS_DEFAULT, nullptr);
+
+    if (publisher_ == nullptr)
+    {
+        return false;
+    }
+
+    topic_ = participant_->create_topic("HelloWorldTopic", "HelloWorld", TOPIC_QOS_DEFAULT);
+
+    if (topic_ == nullptr)
+    {
+        return false;
+    }
+
+    // CREATE THE WRITER
+    writer_ = publisher_->create_datawriter(topic_, wqos, &listener_);
+
+    if (writer_ == nullptr)
+    {
+        return false;
+    }
+    return true;
+}
+
+HelloWorldPublisher::~HelloWorldPublisher()
+{
+    if (writer_ != nullptr)
+    {
+        publisher_->delete_datawriter(writer_);
+    }
+    if (publisher_ != nullptr)
+    {
+        participant_->delete_publisher(publisher_);
+    }
+    if (topic_ != nullptr)
+    {
+        participant_->delete_topic(topic_);
+    }
+    DomainParticipantFactory::get_instance()->delete_participant(participant_);
+}
+
+void HelloWorldPublisher::PubListener::on_publication_matched(
+        eprosima::fastdds::dds::DataWriter*,
+        const eprosima::fastdds::dds::PublicationMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        matched_ = info.total_count;
+        firstConnected_ = true;
+        std::cout << "Publisher matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        matched_ = info.total_count;
+        std::cout << "Publisher unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for PublicationMatchedStatus current count change" << std::endl;
+    }
+}
+
+void HelloWorldPublisher::runThread(
+        uint32_t samples,
+        uint32_t sleep)
+{
+    if (samples == 0)
+    {
+        while (!stop_)
+        {
+            if (publish(false))
+            {
+                // std::cout << "Message: " << hello_.message() << " with index: " << hello_.index()
+                //           << " SENT" << std::endl;
+            }
+            // std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+        }
+
+        std::cout << "publish finished " << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+    else
+    {
+        for (uint32_t i = 0; i < samples; ++i)
+        {
+            if (!publish())
+            {
+                --i;
+            }
+            else
+            {
+                std::cout << "Message: id: " << hello_.id() << " index: " << hello_.index()
+                          << " SENT" << std::endl;
+            }
+            // std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+        }
+
+        std::cout << "publish finished. " << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+}
+
+void HelloWorldPublisher::run(
+        uint32_t samples,
+        uint32_t key,
+        uint32_t sleep)
+{
+    stop_ = false;
+    key_ = key;
+    std::thread thread(&HelloWorldPublisher::runThread, this, samples, sleep);
+    if (samples == 0)
+    {
+        std::cout << "Publisher running. Please press enter to stop the Publisher at any time." << std::endl;
+        std::cin.ignore();
+        stop_ = true;
+    }
+    else
+    {
+        std::cout << "Publisher running " << samples << " samples." << std::endl;
+    }
+    thread.join();
+}
+
+bool HelloWorldPublisher::publish(
+        bool waitForListener)
+{
+    if (listener_.firstConnected_ || !waitForListener || listener_.matched_ > 0)
+    {
+        hello_.id(hello_.id() + 1);  // id is increased from 0 and step is 1.
+        hello_.index(key_);
+        writer_->write(&hello_);
+
+        return true;
+    }
+    return false;
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # fastdds_cpu_usage_test
-This program is used to estimate cpu consumption. 
+This program is used to estimate cpu consumption.   
+
+The original codes are from fastdds/src/fastrtps/examples/C++/DDS/HelloWorldExample.  
+The change is 
+1. Add topic key and 512 bytes array.
+2. User can specify the topic key instead of send interval.
+
+## How to build  
+
+```bash
+$ mkdir build && cd build
+$ cmake ..
+$ make
+```
+
+## Run publisher
+
+```bash
+$ cd build
+$ ./DDSHelloWorldExample publisher 100 1
+```
+100 means the number of sent message. The interval of sending is 100ms.  
+1 means topic key value.  
+
+You can run multi publishers with different topic key.
+
+## Run subscriber
+```bash
+$ cd build
+$ ./DDSHelloWorldExample subscriber
+```
